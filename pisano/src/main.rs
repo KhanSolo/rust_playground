47ba55e033885353477a10b645deb2ab28dd86f2
Konstantin <drpdrp@mail.ru>
2021-09-16 21:45:59 +0300

    TECH: pisano

--- pisano/src/main.rs
+++ pisano/src/main.rs
@@ -1,33 +1,32 @@
 use std::io;
 
-fn pisano_period(m: u64) -> u64 {
+fn pisano_period(m: u128) -> u128 {
     let mut previous = 0;
     let mut current = 1;
-    let mut period = 0u64;
-    for i in 0..=m * m {
-        let tmp1 = current;
-        let tmp2 = (previous + current) % m;
-        previous = tmp1;
-        current = tmp2;
+    let mut period = 0u128;
+    for i in 0..m * m {
+        let tmp = (previous + current) % m;
+        previous = current;
+        current = tmp;
         if previous == 0 && current == 1 {
             period = i + 1;
             break;
         }
     }
-    //println!("pisano_period: {}", period);
+    println!("pisano_period: {}", period);
     period
 }
 
-fn fibonacci_modulo(mut n: u64, m: u64) -> u64 {
+fn fibonacci_modulo(mut n: u128, m: u128) -> u128 {
     let period = pisano_period(m);
     n = n % period;
     let mut previous = 0;
     let mut current = 1;
-    for _ in 0..n-1 {
-        let tmp1 = current;
-        let tmp2 = previous + current;
-        previous = tmp1;
-        current = tmp2;
+    for _ in 0..n-1 {  
+        println!("{} + {}", previous, current);      
+        let tmp = previous + current;
+        previous = current;
+        current = tmp;
     }
     current % m
 }
@@ -36,9 +35,14 @@ fn main() {
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).unwrap();
     let split: Vec<&str> = guess.trim().split(" ").collect();
-    let n: u64 = split[0].trim().parse().unwrap();
-    let m: u64 = split[1].trim().parse().unwrap();
+    let n: u128 = split[0].trim().parse().unwrap();
+    let m: u128 = split[1].trim().parse().unwrap();
 
     let result = fibonacci_modulo(n, m);
     println!("{}", result);
 }
+
+/*
+    (fibonacciModulo(1548276540 235)) #185
+    (fibonacciModulo(11527523930876953 26673)) #10552
+*/
