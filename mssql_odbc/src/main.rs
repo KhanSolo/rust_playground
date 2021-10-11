3c04d7cbe81397355932235d8b3c26f69fab2b93
Konstantin <drpdrp@mail.ru>
2021-10-11 16:39:31 +0300

    CHG: mssql_odbc: fetching data from db

--- mssql_odbc/src/main.rs
+++ mssql_odbc/src/main.rs
@@ -2,6 +2,19 @@ use odbc::*;
 use odbc::ResultSetState::{NoData, Data};
 use std::io;
 
+fn connect() -> std::result::Result<(), DiagnosticRecord> {
+
+    let env = create_environment_v3().map_err(|e| e.unwrap())?;
+
+    let mut buffer = String::new();
+    println!("Please enter connection string: ");
+    // Driver={SQL Server};Server=.;Database=TmsRobots;Uid=plugin;Pwd=Y0djcnm;
+    io::stdin().read_line(&mut buffer).unwrap();
+
+    let conn = env.connect_with_connection_string(&buffer)?;
+    execute_statement(&conn)
+}
+
 fn execute_statement<T: odbc::odbc_safe::AutocommitMode>(conn: &Connection<T>) -> Result<()> {
     let stmt = Statement::with_parent(conn)?;
 
@@ -10,10 +23,30 @@ fn execute_statement<T: odbc::odbc_safe::AutocommitMode>(conn: &Connection<T>) -
     io::stdin().read_line(&mut sql_text).unwrap();
 
     match stmt.exec_direct(&sql_text)? {
-        
+        Data(mut stmt) => {
+            let cols = stmt.num_result_cols()?;
+            while let Some(mut cursor) = stmt.fetch()? {
+                for i in 1..(cols + 1) {
+                    match cursor.get_data::<&str>(i as u16)? {
+                        Some(val) => print!(" {}", val),
+                        None => print!(" NULL"),
+                    }
+                }
+                println!("");
+            }
+        }
+        NoData(_) => println!("Query executed, no data returned"),
     }
+
+    Ok(())
 }
 
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    // todo fix issue with cyrillic symbols
+    match connect() {
+        Ok(()) => println!("Success"),
+        Err(diag) => println!("Error: {}", diag),
+    }
 }
