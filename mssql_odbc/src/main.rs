7bfdd6cdd6e2b30705565b34a4a2b3650a79d78d
Konstantin <drpdrp@mail.ru>
2021-12-29 00:13:39 +0300

    CHG: mssql_odbc

--- mssql_odbc/src/main.rs
+++ mssql_odbc/src/main.rs
@@ -6,7 +6,8 @@ fn connect() -> std::result::Result<(), DiagnosticRecord> {
 
     let env = create_environment_v3().map_err(|e| e.unwrap())?;
 
-    let buffer = String::from("Driver={SQL Server};Server=.;Database=TmsRobots;Uid=plugin;Pwd=Y0djcnm;");
+    let buffer = String::from(
+        "Driver={SQL Server};Server=.;Database=BgtTelegramService;Uid=plugin;Pwd=Y0djcnm;ClientCharset=UTF-8;ServerCharset=UTF-8;");
     //println!("Please enter connection string: ");
     // Driver={SQL Server};Server=.;Database=TmsRobots;Uid=plugin;Pwd=Y0djcnm;
     //io::stdin().read_line(&mut buffer).unwrap();
@@ -18,7 +19,7 @@ fn connect() -> std::result::Result<(), DiagnosticRecord> {
 fn execute_statement<T: odbc::odbc_safe::AutocommitMode>(conn: &Connection<T>) -> Result<()> {
     let stmt = Statement::with_parent(conn)?;
 
-    let sql_text = String::from("select top 1 * from dataexport30");
+    let sql_text = String::from("SELECT TOP 1 * FROM [dbo].[Contact] order by 1 desc");
     //println!("Please enter SQL statement string:");
     //io::stdin().read_line(&mut sql_text).unwrap();
 
@@ -28,6 +29,9 @@ fn execute_statement<T: odbc::odbc_safe::AutocommitMode>(conn: &Connection<T>) -
 
             let cols = stmt.num_result_cols()?;
             while let Some(mut cursor) = stmt.fetch()? {
+
+                let a: &Cursor<Allocated, T> = &cursor;
+
                 for i in 1..(cols + 1) {
                     match cursor.get_data::<&str>(i as u16)? {
                         Some(val) => {
