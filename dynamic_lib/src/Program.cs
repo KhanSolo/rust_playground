443d30067778f79cbd539f9540e96c8c48c1f4e6
Konstantin <drpdrp@mail.ru>
2022-01-01 23:58:47 +0300

    CHG: importing dynamic lib to .net

--- dynamic_lib/src/Program.cs
+++ dynamic_lib/src/Program.cs
@@ -1,10 +1,23 @@
 using System;
+using System.Runtime.InteropServices;
 
 public class Program
 {
+    [DllImport("dynamic_lib.dll")]
+    extern static int sum(int x, int y);
+
     public static void Main()
     {
-        Console.WriteLine("Starting");
-        Console.WriteLine("Exiting");
+        try
+        {
+            Console.WriteLine("Starting");
+            var z = sum(1, 2);
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+        finally
+        {
+            Console.WriteLine("Exiting");
+        }
     }
 }
