756131458d11b5fb1fe5352879be9f77cde7f5c6
Konstantin <drpdrp@mail.ru>
2022-01-06 15:35:30 +0300

    format

--- mocking/src/lib.rs
+++ mocking/src/lib.rs
@@ -6,24 +6,22 @@ pub trait Database {
     fn execute_query(&self, query: String);
 }
 
-pub fn get_user(db: Box<dyn Database>, id: i32){
-    let query = format!(
-        "SELECT * from Users where id={}", id
-    );
+pub fn get_user(db: Box<dyn Database>, id: i32) {
+    let query = format!("SELECT * from Users where id={}", id);
     db.execute_query(query);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test] fn get_user_executes_correct_query() {
+    #[test]
+    fn get_user_executes_correct_query() {
         let mut mock_database = Box::new(MockDatabase::new());
-        mock_database.expect_execute_query()
-        .with(eq(
-            "SELECT * FROM Users where id=22".to_owned()
-        ))
-        .once()
-        .returning(|_x:String|());
+        mock_database
+            .expect_execute_query()
+            .with(eq("SELECT * FROM Users where id=22".to_owned()))
+            .once()
+            .returning(|_x: String| ());
 
         get_user(mock_database, 22);
     }
