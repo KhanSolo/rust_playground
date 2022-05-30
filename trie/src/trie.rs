9786a748d3e1d3c742f1582407f555c86cf9f485
Konstantin Ta <drpdrp@mail.ru>
2022-05-30 03:27:41 +0300

    tech trie

--- trie/src/trie.rs
+++ trie/src/trie.rs
@@ -13,6 +13,8 @@ pub struct Trie {
     head: Node,
 }
 
+fn moving<T>(t:T)->T{t}
+
 impl Trie {
     pub fn new() -> Self {
         Trie { 
@@ -24,11 +26,32 @@ impl Trie {
         }
     }
 
+
+
     pub fn add(&mut self, string: String, value:u32){
-        let h = &self.head;
+
+        let node = &mut self.head;
         for c in string.chars() {
-            //println!("{}", c);
+
+            let mut nd =
+                match moving(node).children.get_key_value(&c) {
+                    Some((_, mut n)) => {
+                        println!("Some {:?}", n);
+                        n
+                    },
+                    None => {
+                        let new_node = Node {
+                            value:None,
+                            max_child_value: None,
+                            children : HashMap::new(),
+                        };
+                        let childs = &mut node.children;
+                        childs.insert(c, new_node);
+                        &mut new_node
+                    },
+                };
+                node = nd;
         }
+        node.value = Some(value);
     }
 }
-
