04990b8e6448819e0a4fe65ec8c75e172b7c8d1a
Konstantin <drpdrp@mail.ru>
2022-06-03 00:26:14 +0300

    CHG: receiving bytes, sending back to socket

--- tokio-chat/src/main.rs
+++ tokio-chat/src/main.rs
@@ -1,7 +1,15 @@
 use tokio::net::TcpListener;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (socket, addr) = listener.accept().await.unwrap();
+    let (mut socket, _) = listener.accept().await.unwrap();
+
+    let mut buf = [0u8;1024];
+    let length = socket.read(&mut buf).await.unwrap();
+
+    // let x: &str = &String::from_utf8_lossy(&buf[..length]);
+    // println!("{} {}", length, x);
+    socket.write_all(&buf[..length]).await.unwrap();
 }
