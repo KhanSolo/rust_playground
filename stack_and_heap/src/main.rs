fefa72c7fb5310706b26ec0cd2eb9d8d46075313
Konstantin Ta <drpdrp@mail.ru>
2022-06-19 13:15:20 +0300

    CHG: experiments with Box

--- stack_and_heap/src/main.rs
+++ stack_and_heap/src/main.rs
@@ -1,16 +1,18 @@
+#[derive(Debug)]
+struct MyStruct{
+    value: i32,
+}
+
+impl MyStruct {
+    fn new(value: i32) -> Self{
+        Self{
+            value
+        }
+    }
+}
+
 fn main() {
-    let a = 2;
-    let result = stack_only(a);
-    dbg!(result);
-}
-
-fn stack_only(b: i32)-> i32{
-    let c = 3;
-    b+c+stack_and_heap()
-}
-
-fn stack_and_heap()-> i32 {
-    let d = 5;
-    let e = Box::new(7);
-    d + *e
+    let a = MyStruct::new(17);
+    let b = Box::new(a);
+    dbg!(*b);
 }
