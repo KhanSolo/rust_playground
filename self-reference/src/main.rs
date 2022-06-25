0e96fbd7a3e916fd9e0f4c325eaf568ed049cf8f
Konstantin Ta <drpdrp@mail.ru>
2022-06-25 20:41:19 +0300

    ADD: self-referencing struct

--- self-reference/src/main.rs
+++ self-reference/src/main.rs
@@ -1,5 +1,11 @@
 use std::fs::read_to_string;
 
+#[derive(Debug)]
+struct S<'a> {
+    input: String,
+    good_lines: Vec<&'a str>,
+}
+
 fn main() {
     let input = match read_to_string("lines.txt") {
         Ok(read) => read,
@@ -9,12 +15,22 @@ fn main() {
         }
     };
 
-    let good_lines: Vec<_> = input
+    let mut s = S {
+        input,
+        good_lines: vec![],
+    };
+
+    s.good_lines = s
+        .input
         .lines()
         .filter(|l| starts_with_capital_letter(l))
         .collect();
 
-    dbg!(good_lines);
+    for line in &s.good_lines {
+        println!("{}", line);
+    }
+
+    dbg!(&s);
 }
 
 fn starts_with_capital_letter(s: &str) -> bool {
