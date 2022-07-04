52d249ebd4b41a6042f7263d6382a190d9f348fd
Konstantin <drpdrp@mail.ru>
2022-07-04 17:18:07 +0300

    ADD: trie more tests

--- trie/src/trie_tests.rs
+++ trie/src/trie_tests.rs
@@ -17,9 +17,39 @@ fn string_absent_substr() {
 }
 
 #[test]
-fn string_present() {
+fn string_exists() {
     let mut trie = trie!(("abc", 1));
     let exists = trie.is_exists("abc");
 
     assert_eq!(exists, true);
+}
+
+#[test]
+fn prefix_notfound(){
+    let mut trie = trie!(("abc", 1));
+    let found = trie.prefix("bcd");
+
+    assert!(found.is_empty());
+}
+
+#[test]
+fn prefix_found_single(){
+    let mut trie = trie!(("abc", 1));
+    let found = trie.prefix("a");
+
+    assert_eq!(found.len(), 1);
+}
+
+#[test]
+fn prefix_found_suitable(){
+    let trie = trie!(
+        ("kare", 10),
+        ("kanojo", 20),
+        ("karetachi", 1),
+        ("korosu", 7),
+        ("sakura", 3)
+    );
+    let found = trie.prefix("k");
+
+    assert_eq!(found.len(), 4);
 }
