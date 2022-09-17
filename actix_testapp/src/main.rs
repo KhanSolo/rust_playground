f1db03d59709a3418502cd4e26e92abfb2647d3c
Konstantin Ta <drpdrp@mail.ru>
2022-09-17 19:16:25 +0300

    ADD: actix_testapp: load appsettings feature

--- actix_testapp/src/main.rs
+++ actix_testapp/src/main.rs
@@ -1,13 +1,29 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use serde::Deserialize;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Deserialize)]
+struct Appsettings {
+    baseurl: String,
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    
+    // Open the file in read-only mode with buffer.
+    let file = File::open("appsettings.json")?;
+    let reader = BufReader::new(file);
+    let serialized:Appsettings = serde_json::from_reader(reader)?;
+    //let addr = "0.0.0.0:8000";
+    let addr = serialized.baseurl;
+
     HttpServer::new(|| {
         App::new()
         .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))
+        .route("/{name}/", web::get().to(greet))
     })
-    .bind("0.0.0.0:8000")?
+    .bind(addr)?
     .run()
     .await
 }
