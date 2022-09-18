504d69a1e1a52cdb4837625871f0e77aab7383e9
Konstantin Ta <drpdrp@mail.ru>
2022-09-19 01:22:53 +0300

    TECH: async_executor

--- async_executor/src/main.rs
+++ async_executor/src/main.rs
@@ -1,3 +1,27 @@
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake},
+};
+use std::{
+    future::Future,
+    sync::mpsc::{sync_channel, Receiver, SyncSender},
+    sync::{Arc, Mutex},
+    task::Context,
+    time::Duration,
+};
+
+//use timer_future::TimerFuture;
+
+// `block_on` blocks the current thread until the provided future has run to
+// completion. Other executors provide more complex behavior, like scheduling
+// multiple futures onto the same thread.
+use futures::executor::block_on;
+
+async fn hello_world() {
+    println!("hello, world!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let future = hello_world(); // Nothing is printed
+    block_on(future); // `future` is run and "hello, world!" is printed
 }
