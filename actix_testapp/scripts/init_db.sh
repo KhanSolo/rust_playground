c07c29b1b9a0ccdfe56cfe6bfcb7d86d671ed5ed
Konstantin <drpdrp@mail.ru>
2022-11-08 22:03:46 +0300

    CHG: postgres port

--- actix_testapp/scripts/init_db.sh
+++ actix_testapp/scripts/init_db.sh
@@ -9,13 +9,13 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=5433}"
 # Launch postgres using Docker
 docker run \
 -e POSTGRES_USER=${DB_USER} \
 -e POSTGRES_PASSWORD=${DB_PASSWORD} \
 -e POSTGRES_DB=${DB_NAME} \
--p "${DB_PORT}":5432 \
+-p "${DB_PORT}":5433 \
 -d postgres \
 postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
