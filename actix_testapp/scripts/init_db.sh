d4ea20a0f793dc43e392682fbf1f15802535b09d
Konstantin <drpdrp@mail.ru>
2022-11-09 21:50:37 +0300

    CHG: postgres waiting

--- actix_testapp/scripts/init_db.sh
+++ actix_testapp/scripts/init_db.sh
@@ -19,3 +19,14 @@ docker run \
 -d postgres \
 postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
+
+#Keep pinging postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "postgres is still unawailable - sleeping"
+    sleep 1
+done
+>&2 echo "postgres is up and running on port ${DB_PORT}"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
