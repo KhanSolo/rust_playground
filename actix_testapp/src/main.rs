ceb2e712c5934ed526da9b999b58bbd9705b0c72
Konstantin Ta <drpdrp@mail.ru>
2022-11-13 23:01:31 +0300

    FIX: actix_testapp build

--- actix_testapp/src/main.rs
+++ actix_testapp/src/main.rs
@@ -4,8 +4,8 @@ use std::net::TcpListener;
 mod appsettings;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {    
-    let app_settings = appsettings::load::load_settings()?;
+async fn main() -> std::io::Result<()> {
+    let app_settings = appsettings::load_settings()?;
     let listener = TcpListener::bind(&app_settings.baseurl).expect("failed to bind");
     run(listener)?.await
 }
